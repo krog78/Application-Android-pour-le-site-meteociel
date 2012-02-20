ee355dbbc7e7290be1204847f3aaad0e7d8a3392
Krog78 <krog78@yahoo.fr>
2012-02-20 22:44:54 +0100

    Initialisation Maven Eclipse

--- Meteociel/src/fr/meteociel/ImageDownloader.java
+++ Meteociel/src/fr/meteociel/ImageDownloader.java
@@ -61,16 +61,17 @@ public class ImageDownloader {
     private final HashMap<String, Bitmap> sHardBitmapCache =
         new LinkedHashMap<String, Bitmap>(HARD_CACHE_CAPACITY / 2, 0.75f, true) {
         private static final long serialVersionUID = -7190622541619388252L;
+       
         @Override
-        protected boolean removeEldestEntry(LinkedHashMap.Entry<String, Bitmap> eldest) {
-            if (size() > HARD_CACHE_CAPACITY) {
-                // Entries push-out of hard reference cache are transferred to soft reference cache
-                sSoftBitmapCache.put(eldest.getKey(), new SoftReference<Bitmap>(eldest.getValue()));
-                return true;
-            } else {
-                return false;
-            }
-        }
+        protected boolean removeEldestEntry(java.util.Map.Entry<String,Bitmap> eldest) {
+        	 if (size() > HARD_CACHE_CAPACITY) {
+                 // Entries push-out of hard reference cache are transferred to soft reference cache
+                 sSoftBitmapCache.put(eldest.getKey(), new SoftReference<Bitmap>(eldest.getValue()));
+                 return true;
+             } else {
+                 return false;
+             }
+        };
     };
 
     // Soft cache for bitmap kicked out of hard cache
